44a047ee9c463ce8c13a8d65356b970eb21f6f8d
Dylan James <dylanzjames14@gmail.com>
2023-07-19 15:26:51 -0500

    Update 2_WKT_Checker.py

--- pages/2_WKT_Checker.py
+++ pages/2_WKT_Checker.py
@@ -1,10 +1,12 @@
 import streamlit as st
 import geopandas as gpd
 import folium
+import os
 from streamlit_folium import folium_static
 from shapely import wkt
 from shapely.geometry import Polygon
 from pyproj import CRS
+from tempfile import NamedTemporaryFile
 
 def calculate_statistics(gdf, area_unit):
     # Reproject to suitable CRS for distance/area measurements
@@ -32,6 +34,9 @@ wkt_input = st.text_area("Enter WKT string:", "")
 # Dropdown for area unit
 area_unit = st.selectbox("Select unit for area:", ("square meters", "hectares", "acres"))
 
+# Dropdown for file format
+file_format = st.selectbox("Select file format for download:", ("GeoJSON", "KML"))
+
 # Parse WKT
 if wkt_input:
     try:
@@ -53,5 +58,18 @@ if wkt_input:
         st.write(f"Area: {area} {area_unit}")
         st.write(f"Perimeter: {perimeter} meters")
         st.write(f"Number of Polygons: {count}")
+
+        # Create temporary file for download
+        tmpfile = NamedTemporaryFile(delete=False)
+
+        # Export to selected file format
+        if file_format == "GeoJSON":
+            gdf.to_file(tmpfile.name, driver='GeoJSON')
+        else:  # KML
+            gdf.to_file(tmpfile.name, driver='KML')
+
+        # Create download link
+        st.markdown(f"[Download {file_format} file]({tmpfile.name})")
+
     except Exception as e:
         st.write(f"Error: {str(e)}")
