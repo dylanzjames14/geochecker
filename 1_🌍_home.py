6ed8eea0705b1af62bfbee0e20a3ce58058eba43
Dylan James <dylanzjames14@gmail.com>
2023-07-18 21:53:50 -0500

    Update and rename home.py to 1_🌍_home.py

---
+++ 1_🌍_home.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(page_title="Geochecker", page_icon="🌍", layout="wide")
+st.title("Welcome to Geochecker!")
+st.write("Geochecker is a tool for analyzing common geospatial files and formats!")
