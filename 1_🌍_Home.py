ccfedcebb0c122ace7504498bc00f665ab9c6ce8
Dylan James <dylanzjames14@gmail.com>
2023-07-23 16:20:18 -0500

    Update 1_🌍_Home.py

--- 1_🌍_Home.py
+++ 1_🌍_Home.py
@@ -1,6 +1,14 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+from PIL import Image
 
 st.set_page_config(page_title="Spatial Annihilator", page_icon="🌍", layout="wide")
+
+# Open the image file
+img = Image.open('/data/image.png')
+
 st.title("Welcome to Spatial Annihilator!")
 st.write("A tool for analyzing common geospatial files and formats!")
+
+# Display the image
+st.image(img, use_column_width=True)
