9c0659358589ad708d60be13e0fd3194dde0f05c
Dylan James <dylanzjames14@gmail.com>
2023-07-23 10:25:21 -0500

    Update 1_🌍_Home.py

--- 1_🌍_Home.py
+++ 1_🌍_Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-st.set_page_config(page_title="Geochecker", page_icon="🌍", layout="wide")
-st.title("Welcome to Geochecker!")
-st.write("Geochecker is a tool for analyzing common geospatial files and formats!")
+st.set_page_config(page_title="Spatial Annihilator", page_icon="🌍", layout="wide")
+st.title("Welcome to Spatial Annihilator!")
+st.write("A tool for analyzing common geospatial files and formats!")
